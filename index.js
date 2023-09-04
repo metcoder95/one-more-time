5c8a096b9853d5c179a171f6a59be4ed08c8a008
Carlos Fuentes <me@metcoder.dev>
2023-09-04 18:08:12 +0200

    feat: initial version

--- index.js
+++ index.js
@@ -1,1 +1,134 @@
 'use strict'
+const { EventEmitter } = require('events')
+
+const { kInternalIDCounter, kTaskOptions } = require('./lib/symbols')
+
+const { getDeferredPromise } = require('./lib/utils')
+
+const { Task } = require('./lib/task')
+
+class Retry extends EventEmitter {
+  constructor ({
+    maxTimeout, // In seconds
+    minTimeout, // In seconds
+    factor,
+    retries
+  } = {}) {
+    if (
+      maxTimeout != null &&
+      (typeof maxTimeout !== 'number' || maxTimeout < 1)
+    ) {
+      throw new TypeError('maxTimeout must be positive integer')
+    }
+
+    if (
+      minTimeout != null &&
+      (typeof minTimeout !== 'number' ||
+        minTimeout < 1 ||
+        minTimeout > maxTimeout)
+    ) {
+      throw new TypeError(
+        'minTimeout must be a positive integer and less than maxTimeout'
+      )
+    }
+
+    if (factor != null && (typeof factor !== 'number' || factor < 1)) {
+      throw new TypeError('factor must be a positive integer')
+    }
+
+    if (retries != null && (typeof retries !== 'number' || retries < 1)) {
+      throw new TypeError('retries must be a positive integer')
+    }
+
+    super('OneMoreTime#Retry')
+
+    this[kTaskOptions] = {
+      maxTimeout: maxTimeout ?? 30 * 1000, // In milliseconds
+      minTimeout: minTimeout ?? 500,
+      factor: factor ?? 2,
+      retries: retries ?? 3
+    }
+
+    this[kInternalIDCounter] = 0
+  }
+
+  get config () {
+    return { ...this[kTaskOptions] }
+  }
+
+  pick ({ id, signal, retries, currentTimeout } = {}) {
+    if (id != null && typeof id !== 'string') {
+      throw new TypeError('id must be a string')
+    }
+
+    if (
+      signal != null &&
+      typeof signal.on !== 'function' &&
+      typeof signal.addEventListener !== 'function'
+    ) {
+      throw new TypeError('invalid signal')
+    }
+
+    const taskId = id ?? `task-${this[kInternalIDCounter]++}`
+    const task = new Task(this, { id: taskId, signal, retries, currentTimeout })
+
+    return task
+  }
+
+  run (fn, { shouldRetry, ...taskOpts } = {}) {
+    if (typeof fn !== 'function') {
+      throw new TypeError('fn must be a function')
+    }
+
+    if (shouldRetry != null && typeof shouldRetry !== 'function') {
+      throw new TypeError('shouldRetry must be a function')
+    }
+
+    shouldRetry = shouldRetry ?? (() => true)
+
+    const task = this.pick(taskOpts)
+
+    return exec()
+
+    function exec (promise, resolve, reject) {
+      if (promise == null) {
+        ;({ resolve, reject, promise } = getDeferredPromise())
+      }
+
+      try {
+        const res = fn(task)
+
+        if (res.then != null && typeof res.then === 'function') {
+          return res.then(
+            (...args) => {
+              resolve(...args)
+            },
+            error => {
+              if (shouldRetry(error) && task.shouldRetry(error)) {
+                return task.timeout().then(() => exec(promise, resolve, reject))
+              }
+
+              throw error
+            }
+          )
+        }
+
+        resolve(res)
+      } catch (error) {
+        if (shouldRetry(error) && task.shouldRetry(error)) {
+          return task.timeout().then(() => exec(promise, resolve, reject))
+        }
+
+        reject(error)
+      }
+
+      return promise
+    }
+  }
+
+  static Retry = Retry
+
+  static default = Retry
+}
+
+module.exports = Retry
--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,164 @@
 'use strict'
+const { EventEmitter } = require('events')
+
+const { test } = require('tap')
+
+const { Task } = require('../lib/task')
+
+const Retry = require('..')
+
+test('Retry#Module', suite => {
+  suite.plan(3)
+  suite.comment(
+    'Any breaking change in this suite should be considered a major version bump.'
+  )
+
+  suite.test('Should export Retry class', t => {
+    t.plan(4)
+
+    t.equal(typeof Retry, 'function')
+    t.ok(Retry.prototype instanceof EventEmitter)
+    t.equal(Retry, Retry.Retry)
+    t.equal(Retry.default, Retry)
+  })
+
+  suite.test('Instances', t => {
+    const retry = new Retry()
+    const retry2 = new Retry.Retry()
+    // eslint-disable-next-line new-cap
+    const retry3 = new Retry.default()
+
+    t.plan(3)
+    t.type(retry, Retry)
+    t.type(retry2, Retry)
+    t.type(retry3, Retry)
+  })
+
+  suite.test('Should expose right APIs', t => {
+    const retry = new Retry()
+    const config = {
+      maxTimeout: 30 * 1000, // In milliseconds
+      minTimeout: 500,
+      factor: 2,
+      retries: 3
+    }
+
+    t.plan(6)
+
+    t.match(retry.config, config)
+    retry.config.factor = 1
+    t.match(retry.config, config)
+
+    t.equal(typeof retry.pick, 'function')
+    t.equal(retry.pick.length, 0)
+    t.equal(typeof retry.run, 'function')
+    t.equal(retry.run.length, 1)
+  })
+})
+
+test('Retry#pick', suite => {
+  suite.plan(2)
+
+  suite.test('Should return a task', t => {
+    const retry = new Retry()
+
+    t.plan(2)
+
+    t.type(retry.pick(), Task)
+    t.type(retry.pick({}), Task)
+  })
+
+  suite.test('Should return a task with right defaults', t => {
+    const retry = new Retry()
+
+    t.plan(2)
+
+    const task = retry.pick({
+      id: 'foo',
+      signal: new AbortController().signal,
+      retries: 1,
+      currentTimeout: 1000
+    })
+
+    t.equal(task.id, 'foo')
+    t.type(task, Task)
+  })
+})
+
+test('Retry#run', suite => {
+  suite.plan(3)
+
+  suite.test('Should execute a given function', async t => {
+    let counter = 0
+    let retryCounter = 0
+    const foo = () => {
+      counter++
+
+      if (counter !== 3) {
+        throw new Error('foo')
+      } else {
+        return 'bar'
+      }
+    }
+    const shouldRetry = () => {
+      retryCounter++
+      return true
+    }
+
+    t.plan(3)
+    const retry = new Retry()
+    const result = await retry.run(foo, { shouldRetry })
+
+    t.equal(result, 'bar')
+    t.equal(counter, 3)
+    t.equal(retryCounter, 2)
+  })
+
+  suite.test(
+    'Should execute a given function with state overrides',
+    async t => {
+      let counter = 0
+      let retryCounter = 0
+      const foo = () => {
+        counter++
+
+        if (counter !== 3) {
+          throw new Error('foo')
+        } else {
+          return 'bar'
+        }
+      }
+      const shouldRetry = () => {
+        retryCounter++
+        return true
+      }
+
+      t.plan(3)
+      const retry = new Retry()
+      const result = await retry.run(foo, {
+        shouldRetry,
+        retries: 1,
+        currentTimeout: 1000
+      })
+
+      t.equal(result, 'bar')
+      t.equal(counter, 3)
+      t.equal(retryCounter, 2)
+    }
+  )
+
+  suite.test('Should exhaust retries for a given function', async t => {
+    let counter = 0
+    const foo = () => {
+      counter++
+
+      throw new Error('foo')
+    }
+
+    t.plan(2)
+    const retry = new Retry()
+    await t.rejects(retry.run.bind(retry, foo), 'foo')
+
+    t.equal(counter, 4)
+  })
+})
