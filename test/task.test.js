b181ea917de53929e3ca454d9d864c83b0ede663
Carlos Fuentes <me@metcoder.dev>
2023-09-05 14:07:00 +0200

    test: add more testing for Tasks

--- test/task.test.js
+++ test/task.test.js
@@ -6,7 +6,7 @@ const { Task } = require('../lib/task')
 const Retry = require('..')
 
 test('Retry/Task', suite => {
-  suite.plan(1)
+  suite.plan(4)
   suite.comment(
     'Any breaking change in this suite should be considered a major version bump.'
   )
@@ -15,7 +15,7 @@ test('Retry/Task', suite => {
     const retry = new Retry()
     const task = retry.pick()
 
-    t.plan(12)
+    t.plan(13)
     t.type(task, Task)
     t.equal(typeof task.start, 'function')
     t.equal(task.start.length, 0)
@@ -28,10 +28,162 @@ test('Retry/Task', suite => {
     t.equal(task.retries, 0)
     t.notOk(task.rootError)
     t.match(task.history, [])
+    t.notOk(task.aborted)
+  })
+
+  suite.test(
+    'Should not allow fresh start if task already aborted',
+    { skip: (AbortController || null) == null },
+    t => {
+      const retry = new Retry()
+      const controller = new AbortController()
+      const task = retry.pick({ signal: controller.signal })
+
+      controller.abort()
+      t.plan(1)
+
+      t.throws(
+        () => task.start(),
+        new Error('Task aborted. Did you forget to call "reset" first?')
+      )
+    }
+  )
+
+  suite.test('Should not allow fresh start if task already started', t => {
+    const retry = new Retry()
+    const task = retry.pick({ retries: 3 })
+
+    t.plan(1)
+    t.throws(
+      () => task.start(),
+      new Error('Task already started. Did you forget to call "reset" first?')
+    )
+  })
+
+  suite.test('Should throw if bad signal passed', t => {
+    const retry = new Retry()
+    const task = retry.pick()
+
+    t.plan(1)
+    t.throws(() => task.start('bad signal'), new TypeError('invalid signal'))
   })
 })
 
-test('Retry/Task', { todo: true })
+test('Retry/Task', suite => {
+  suite.plan(3)
+
+  suite.test('Should enable granular flow control', async t => {
+    let counter = 0
+    const retry = new Retry()
+    const task = retry.pick()
+    const expectedError = new Error('foo')
+    const fn = () => {
+      counter++
+
+      if (counter < 4) {
+        throw expectedError
+      }
+    }
+
+    t.plan(4)
+
+    while (true) {
+      try {
+        fn()
+        break
+      } catch (err) {
+        if (task.shouldRetry(err)) {
+          await task.timeout()
+          continue
+        }
+
+        throw err
+      }
+    }
+
+    t.equal(counter, 4)
+    t.equal(task.retries, 3)
+    t.equal(task.history.length, 3)
+    t.equal(task.rootError, expectedError)
+  })
+
+  suite.test(
+    'Should not retry if task aborted',
+    { skip: (AbortController || null) == null },
+    async t => {
+      let counter = 0
+      const retry = new Retry()
+      const controller = new AbortController()
+      const task = retry.pick({ signal: controller.signal })
+      const expectedError = new Error('foo')
+      const fn = () => {
+        counter++
+
+        if (counter < 4) {
+          throw expectedError
+        }
+      }
+
+      t.plan(5)
+
+      controller.abort()
+
+      try {
+        fn()
+      } catch (err) {
+        t.notOk(task.shouldRetry(err))
+      }
+
+      t.equal(counter, 1)
+      t.equal(task.retries, 0)
+      t.equal(task.history.length, 1)
+      t.equal(task.rootError, expectedError)
+    }
+  )
+
+  suite.test(
+    'Should not retry if task aborted',
+    { skip: (AbortController || null) == null },
+    async t => {
+      let counter = 0
+      const retry = new Retry()
+      const controller = new AbortController()
+      const task = retry.pick({ signal: controller.signal })
+      const expectedError = new Error('foo')
+      const fn = () => {
+        counter++
+
+        if (counter < 3) {
+          throw expectedError
+        }
+
+        controller.abort()
+      }
+
+      t.plan(5)
+
+      while (true) {
+        try {
+          fn()
+          break
+        } catch (err) {
+          if (task.shouldRetry(err)) {
+            await task.timeout()
+            continue
+          }
+
+          break
+        }
+      }
+
+      t.ok(task.aborted)
+      t.equal(counter, 3)
+      t.equal(task.retries, 2)
+      t.equal(task.history.length, 2)
+      t.equal(task.rootError, expectedError)
+    }
+  )
+})
 
 test('Retry/Task#Reset', { todo: true })
 test('Retry/Task#Custom State', { todo: true })
